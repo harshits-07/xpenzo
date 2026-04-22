abed0dc6fc1e5e1f378116348a783b2d01c5974c
Harshit Sharma <harshit.belovance@gmail.com>
2026-04-22 21:46:34 +0530

    folder structure is done -expense model file is done.

---
+++ lib/models/expense_model.dart
@@ -0,0 +1,39 @@
+class ExpenseModel {
+  String id;
+  String title;
+  double amount;
+  String category;
+  DateTime date;
+  String userId;
+
+  ExpenseModel({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.category,
+    required this.date,
+    required this.userId,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'amount': amount,
+      'category': category,
+      'date': date.toIso8601String(),
+      'userId': userId,
+    };
+  }
+
+  factory ExpenseModel.fromMap(Map<String, dynamic> map) {
+    return ExpenseModel(
+      id: map['id'],
+      title: map['title'],
+      amount: map['amount'],
+      category: map['category'],
+      date: DateTime.parse(map['date']),
+      userId: map['userId'],
+    );
+  }
+}
